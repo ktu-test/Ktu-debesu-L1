17d288e6d4821067775dd3a45e9057dd4a029ec5
Tadas <tadbask@gmail.com>
2014-10-22 22:44:55 +0300

    form

--- test.php
+++ test.php
@@ -1,12 +1,15 @@
-<?php
-
-use google\appengine\api\users\User;
-use google\appengine\api\users\UserService;
-# Looks for current Google account session
-$user = UserService::getCurrentUser();
-if ($user) {
-  echo 'Hello, ' . htmlspecialchars($user->getNickname());
-}
-else {
-  header('Location: ' . UserService::createLoginURL($_SERVER['REQUEST_URI']));
-}
+<html>
+  <body>
+    <?php
+    if (array_key_exists('content', $_POST)) {
+      echo "You wrote:<pre>\n";
+      echo htmlspecialchars($_POST['content']);
+      echo "\n</pre>";
+    }
+    ?>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
